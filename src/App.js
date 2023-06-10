f96104030aea90e3f7cbe59d9dc0c648c23ce4d1
SergeyHak <hakimzanov92@mail.ru>
2023-06-10 22:31:19 +0600

    add-content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [value, setValue] = useState("");
+  useEffect(() => {
+    const getToDo = async () => {
+      const data = await (
+        await fetch("https://wedev-api.sky.pro/api/todos")
+      ).json();
+      setTodo(data);
+    };
+    getToDo();
+  }, []);
+
+  const addToDo = async () => {
+    const data = await (
+      await fetch("https://wedev-api.sky.pro/api/todos", {
+        method: "POST",
+        mode: "cors",
+        body: JSON.stringify({
+          text: value,
+        }),
+      })
+    ).json();
+    setTodo(data);
+    console.log(data)
+  };
+  const deleteToDo = async (id) => {
+    const data = await (
+      await fetch (`https://wedev-api.sky.pro/api/todos/${id}`,{
+        method:"DELETE",
+      })      
+       ).json();
+       console.log(data)       
+       setTodo(data);
+      };
+      function addNewToDo(params) {
+        addToDo();
+        setValue('')
+      }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ol>
+        {todo.todos?.map((item) => (
+          <div  className="todo-content">
+            <li key={item.text}>{item.text}</li>
+            <button key={item.id}  onClick={()=>deleteToDo(item.id)}>Удалить задачу</button>
+          </div>
+        ))}
+        <input
+          type="text"
+          value={value}
+          placeholder="Введите знвчение"
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        <button onClick={addNewToDo}>Добавить задачу</button>
+      </ol>
     </div>
   );
 }
